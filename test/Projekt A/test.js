e7c059028baa2eed74ad304897c1ad4a7b555087
firsttris <tristanteufel@googlemail.com>
2019-12-15 16:03:55 +0100

    test projects

---
+++ test/Projekt A/test.js
@@ -0,0 +1,12 @@
+const sum = require('./sum');
+describe('.save()', () => {
+
+it('save', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('save stuff', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+});
