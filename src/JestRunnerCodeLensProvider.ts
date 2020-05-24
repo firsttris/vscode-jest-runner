9a1ccc583fa0c14d2c543a134884ac6c2743894c
Estefania Guardado <estefania.cguardado@gmail.com>
2020-05-24 17:05:12 +0200

    feat: recursive test childrens

--- src/JestRunnerCodeLensProvider.ts
+++ src/JestRunnerCodeLensProvider.ts
@@ -1,18 +1,41 @@
-import { parse } from 'jest-editor-support';
+import { parse, ParsedNode } from 'jest-editor-support';
 import { CodeLens, CodeLensProvider, Range, TextDocument } from 'vscode';
+
+function getTestsBlocks(parsedNode: ParsedNode): CodeLens[] {
+  const codeLens: CodeLens[] = [];
+
+  parsedNode.children?.forEach(subNode => {
+    codeLens.push(...getTestsBlocks(subNode));
+  });
+
+  const range = new Range(
+    parsedNode.start.line - 1,
+    parsedNode.start.column,
+    parsedNode.end.line - 1,
+    parsedNode.end.column
+  );
+
+  if (parsedNode.type === 'expect') {
+    return [];
+  }
+
+  codeLens.push(
+    new CodeLens(range, {
+      arguments: [parsedNode['name']],
+      command: 'extension.runJest',
+      title: 'Run test'
+    })
+  );
+
+  return codeLens;
+}
 
 export default class JestRunnerCodeLensProvider implements CodeLensProvider {
   public async provideCodeLenses(document: TextDocument): Promise<CodeLens[]> {
-    const parseResults = parse(document.fileName).root.children;
+    const parseResults = parse(document.fileName, document.getText()).root.children;
 
-    return parseResults.map(test => {
-      const range = new Range(test.start.line - 1, test.start.column, test.end.line - 1, test.end.column);
-
-      return new CodeLens(range, {
-        arguments: [test['name']],
-        command: 'extension.runJest',
-        title: 'Run test'
-      });
-    });
+    const codeLens = [];
+    parseResults.forEach(parseResult => codeLens.push(...getTestsBlocks(parseResult)));
+    return codeLens;
   }
 }
