622c3427d35a34293dd638fc2589cc048ff7f669
Lucas Oliveira <loliveira@esss.co>
2021-04-23 14:40:53 -0300

    Define commands for runInPath and debugInPath

--- src/extension.ts
+++ src/extension.ts
@@ -9,28 +9,30 @@ export function activate(context: vscode.ExtensionContext) {
   const codeLensProvider = new JestRunnerCodeLensProvider();
   const config = new JestRunnerConfig();
 
-  const runJest = vscode.commands.registerCommand('extension.runJest', async (argument: object | string | {path: string}) => {
+  const runJest = vscode.commands.registerCommand('extension.runJest', async (argument: object | string ) => {
     if (typeof argument === 'string') {
       jestRunner.runCurrentTest(argument);
-    } else if('path' in argument) {
-      jestRunner.runTestsOnPath(argument.path)
     } else {
       jestRunner.runCurrentTest();
     }
   });
+  const runJestPath = vscode.commands.registerCommand('extension.runJestPath', async (argument: vscode.Uri) => 
+    jestRunner.runTestsOnPath(argument.path)
+  );
   const runJestAndUpdateSnapshots = vscode.commands.registerCommand('extension.runJestAndUpdateSnapshots', async () => {
       jestRunner.runCurrentTest('', ['-u']);
   });
   const runJestFile = vscode.commands.registerCommand('extension.runJestFile', async () => jestRunner.runCurrentFile());
-  const debugJest = vscode.commands.registerCommand('extension.debugJest', async (argument: object | string | {path: string}) => {
+  const debugJest = vscode.commands.registerCommand('extension.debugJest', async (argument: object | string ) => {
     if (typeof argument === 'string') {
       jestRunner.debugCurrentTest(argument)
-    } else if('path' in argument) {
-      jestRunner.debugTestsOnPath(argument.path);
     } else {
       jestRunner.debugCurrentTest()
     }
   });
+  const debugJestPath = vscode.commands.registerCommand('extension.debugJestPath', async (argument: vscode.Uri) => 
+    jestRunner.debugTestsOnPath(argument.path)
+  );
   const runPrev = vscode.commands.registerCommand('extension.runPrevJest', async () => jestRunner.runPreviousTest());
   const runJestFileWithCoverage = vscode.commands.registerCommand('extension.runJestFileWithCoverage', async () =>
     jestRunner.runCurrentFile(['--coverage'])
@@ -46,7 +48,9 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(runJest);
   context.subscriptions.push(runJestAndUpdateSnapshots);
   context.subscriptions.push(runJestFile);
+  context.subscriptions.push(runJestPath);
   context.subscriptions.push(debugJest);
+  context.subscriptions.push(debugJestPath);
   context.subscriptions.push(runPrev);
   context.subscriptions.push(runJestFileWithCoverage);
 }
