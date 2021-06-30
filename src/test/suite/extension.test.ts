b64db60a1eb970599f8141929c09c3c5618eaeed
koichi.sakamoto <sakamoto0321@gmail.com>
2021-06-30 23:25:34 +0900

    add test case

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -6,14 +6,23 @@ import * as vscode from 'vscode';
 import { TextEncoder } from 'util';
 import { describe, it } from 'mocha';
 
-describe('Extension Test Suite (command)', () => {
-	vscode.window.showInformationMessage('Start command tests.');
+describe('Extension Commands', async () => {
+	vscode.window.showInformationMessage('Start config tests.');
+	const conf = vscode.workspace.getConfiguration('playwrightrunner');
+	let rootDir:vscode.Uri = vscode.Uri.file('.'); 
+	if(undefined !== vscode.workspace.workspaceFolders && 0 < vscode.workspace.workspaceFolders.length){
+		rootDir = vscode.workspace.workspaceFolders[0] && vscode.workspace.workspaceFolders[0].uri;
+	}
+	//const assetRootDir = rootDir.fsPath.replace(/\\/g, '/');
 
 	it('setup', async () => {
 
 	}).timeout(10000);
 
-	it('command vscode-paste-image.pasteImage test', async () => {
+	it('playwright.runTest', async () => {
+		const fpath = vscode.Uri.joinPath(rootDir, "api-test/test2.test.js");
+		const editor = await vscode.window.showTextDocument(fpath, { preview: false });
+		await vscode.commands.executeCommand("playwright.runTest");
 	}).timeout(10000);
 
 	it('command vscode-paste-image.pasteBase64Image test', async () => {
