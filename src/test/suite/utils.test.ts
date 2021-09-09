d727e345b3867c3fea582ed79d3aa4ade49d9001
antoine jubin <antoine.jubin@depinfonancy.net>
2021-09-09 18:44:38 +0200

    test(utils): add test case on validate codelens options

---
+++ src/test/suite/utils.test.ts
@@ -0,0 +1,12 @@
+import { validateCodeLensOptions } from '../../util';
+
+describe('validateCodeLensOptions', () =>
+  it.each([
+    [
+      ['a', 'run', 'RUN', 'watch', 'debug', 'other', 'debug', 'debug', 'watch', 'run'],
+      ['run', 'watch', 'debug'],
+    ],
+    [[], []],
+  ])('should turn "jestrunner.codeLens" options  into something valid', (input, expected) => {
+    expect(validateCodeLensOptions(input)).toEqual(expected);
+  }));
