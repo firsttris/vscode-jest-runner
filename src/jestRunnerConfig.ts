aef38149d6d8c0becb675ec6da05e30c34b374a5
Nir Gilboa <gilboa.nir@gmail.com>
2022-02-26 21:27:42 +0200

    fix: missing import for CodeLensOption (#232)

--- src/jestRunnerConfig.ts
+++ src/jestRunnerConfig.ts
@@ -1,7 +1,7 @@
 import * as path from 'path';
 import * as fs from 'fs';
 import * as vscode from 'vscode';
-import { isWindows, normalizePath, quote, validateCodeLensOptions } from './util';
+import { CodeLensOption, isWindows, normalizePath, quote, validateCodeLensOptions } from './util';
 
 export class JestRunnerConfig {
   /**
