09fed25787fab37320ebe7ea5f69280cc6df4ec5
Dom Slee <domslee1@gmail.com>
2023-10-15 16:46:10 +1100

    Update deps to fix audit issues (#337)

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ const config = {
     rules: [
       {
         test: /\.ts$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules|test)/,
         use: [
           {
             loader: 'ts-loader',
